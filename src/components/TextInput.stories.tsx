196c2057f423a3c3b9f32eaf28030daeff02a685
Christopher Alves <christopher.alves@portalsolar.com.br>
2022-10-14 07:21:13 -0300

    first commit

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Envelope } from "phosphor-react";
+
+import { TextInput, TextInputRootProps } from "./TextInput";
+
+export default {
+  title: "Components/TextInput",
+  component: TextInput.Root,
+  args: {
+    children: [
+      //Colocado dentro de um array para que no storybook não fique aparecendo o ReactFragment que seria necessário
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />,
+    ],
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputRootProps> = {};
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: <TextInput.Input placeholder="Type your e-mail address" />,
+  },
+};
